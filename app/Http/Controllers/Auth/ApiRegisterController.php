eff3a38dc1a231b11119a462c323f4ed3ac50c83
ZZW <cofahoher@163.com>
2018-01-02 15:42:05 +0800

    异常

--- app/Http/Controllers/Auth/ApiRegisterController.php
+++ app/Http/Controllers/Auth/ApiRegisterController.php
@@ -5,54 +5,26 @@ namespace App\Http\Controllers\Auth;
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Database\QueryException;
+
+
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
 class ApiRegisterController extends Controller
 {
-    use RegistersUsers;
-
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
-
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => 'required|string|max:20',
-            'email' => 'required|string|email|max:50|unique:users',
-            'password' => 'required|string|min:6|confirmed',
-        ]);
-    }
-
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\User
-     */
     protected function create(Request $request)
     {
         $crc = \CRC::crc64($request->input('email'));
+        /*
+        return User::create([
+                'id_crc64' => $crc,
+                'email' => $request->input('email'),
+                'name' => $request->input('name'),
+                'password' => bcrypt($request->input('password')),
+            ]);
+        */
+        $result = NULL;
         try
         {
             $result = User::create([
@@ -62,14 +34,15 @@ class ApiRegisterController extends Controller
                 'password' => bcrypt($request->input('password')),
             ]);
         }
-        catch (Exception $e)
+        catch (QueryException $e)
         {
-            return 'doesn"t come here';
+            return 'email exists';
         }
         finally
         {
-            return 'error';
         }
+        if ($result == NULL)
+            return 'error';
         return 'success';
     }
 }
